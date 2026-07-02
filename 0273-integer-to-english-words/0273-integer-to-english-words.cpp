55f7de72fea772c3c4bf03f0464ee1d58ca1d85a
Nadella Shyamanth <shyamanthnadella.03082007@gmail.com>
2026-07-03 02:48:24 +0530

    Time: 3 ms (48.34%), Space: 11.9 MB (31.38%) - LeetHub

---
+++ 0273-integer-to-english-words/0273-integer-to-english-words.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    vector<string> below20 = {"", "One", "Two", "Three", "Four", "Five", "Six","Seven", "Eight", "Nine", "Ten", "Eleven",   "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
+    vector<string> tens = {"", "", "Twenty", "Thirty", "Forty", "Fifty","Sixty", "Seventy", "Eighty", "Ninety"};
+    string helper(int num) {
+        if (num == 0) return "";
+        if (num < 20) 
+            return below20[num]+" ";
+        else if (num<100) 
+            return tens[num/10]+" "+helper(num%10);
+        else 
+            return below20[num/100]+" Hundred "+helper(num%100);
+    }
+
+    string numberToWords(int num) {
+        if(num==0) return "Zero";
+        string result = "";
+        if (num>=1000000000) {
+            result+=helper(num/1000000000)+"Billion ";
+            num%=1000000000;
+        }
+        if (num>=1000000) {
+            result+=helper(num/1000000)+"Million ";
+            num %=1000000;
+        }
+        if (num >= 1000) {
+            result+=helper(num/1000) +"Thousand ";
+            num%=1000;
+        }
+        if (num>0){
+            result+=helper(num);
+        }
+        if (!result.empty() && result.back() == ' ')
+            result.pop_back();
+        return result;
+    }
+};
